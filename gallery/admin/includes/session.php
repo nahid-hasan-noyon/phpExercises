ef0341ff849374467d303d986f772be4ca9adc87
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-04-20 14:55:49 +0600

    showing feedback when wrong password is entered

--- gallery/admin/includes/session.php
+++ gallery/admin/includes/session.php
@@ -5,11 +5,13 @@ class Session
 
     private $is_signed = false;
     public $user_id;
+    public $message;
 
     function __construct()
     {
         session_start();
         $this->check_the_login();
+        $this->check_message();
     }
 
     private function check_the_login()
@@ -41,6 +43,21 @@ class Session
         unset($user_id);
         unset($_SESSION['user_id']);
         $this->is_signed = false;
+    }
+
+    public function message($msg = "")
+    {
+        if (!empty($msg)) {
+            $_SESSION['message'] = $msg;
+        } else
+            return $this->message;
+    }
+    private function check_message()
+    {
+        if (isset($_SESSION['message'])) {
+            $this->message = $_SESSION['message'];
+        } else
+            $this->message = "";
     }
 }
 
--- gallery/admin/login.php
+++ gallery/admin/login.php
@@ -19,6 +19,7 @@ if (isset($_POST['submit'])) {
         $the_message = "Your username or password is incorrect.";
     }
 } else {
+    $the_message = "";
     $username = "";
     $password = "";
 }
@@ -26,15 +27,15 @@ if (isset($_POST['submit'])) {
 ?>
 
 <div class="col-md-4 col-md-offset-4">
+    <h4 class="bg-danger"><?php echo $the_message; ?></h4>
     <form action="" method="post">
         <div class="form-group">
             <label for="username">Username</label>
-            <input type="text" class="form-control" name="username">
-
+            <input type="text" class="form-control" name="username" value="<?php echo htmlentities($username); ?>">
         </div>
         <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" class="form-control" name="password">
+            <input type="password" class="form-control" name="password" value="<?php echo htmlentities($password); ?>">
         </div>
         <div class="form-group">
             <input type="submit" value="Submit" name="Submit" class="btn btn-primary">
